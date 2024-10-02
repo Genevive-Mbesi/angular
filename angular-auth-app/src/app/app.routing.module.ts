12440784fb37e481dbad8cbe120f2165f5c920d3
mbesi <genmbesi@gmail.com>
2024-10-02 08:34:46 +0300

    login form set up

--- angular-auth-app/src/app/app.routing.module.ts
+++ angular-auth-app/src/app/app.routing.module.ts
@@ -3,7 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 
-const routes: Routes = [
+export const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'dashboard', component: DashboardComponent },
   { path: '', redirectTo: '/login', pathMatch: 'full' } // default route
